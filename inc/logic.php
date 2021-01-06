76ae1b83a3f6be1c6946e4eab66c8e9e677ac2c2
NizamovArturV <shurshio11@gmail.com>
2021-01-06 21:34:53 +0500

    Исправления по комментариям

--- inc/logic.php
+++ inc/logic.php
@@ -1,46 +1,76 @@
 <?php
 
-if (isset($_FILES['userfile']) && isset($_POST['upload'])) {
-    //Преобразуем массив файлов
-    foreach ($_FILES['userfile'] as $key => $file) {
+// Преобразование массива
+function FilesArr(&$filesArr)
+{
+    foreach ($filesArr as $key => $file) {
         foreach ($file as $k => $v) {
-            $_FILES['userfile'][$k][$key] = $v;
+            $filesArr[$k][$key] = $v;
         }
-        unset ($_FILES['userfile'][$key]);
+        unset ($filesArr[$key]);
     }
+    return $filesArr;
+}
+
+// Количество файлов
+function CountFiles($filesArr)
+{
+    return (count($filesArr) > 5) ? false : true;
+}
+
+// Проверка ошибок 
+function CheckErrors($fileError)
+{
+    return ($fileError != 0) ? false : true;
+}
+
+//Проверка типа файла 
+function CheckType($fileType, $fileTypesAccept = ['image/jpeg', 'image/png', 'image/jpg'])
+{
+    return (!in_array($fileType,$fileTypesAccept)) ? false : true;
+}
+
+//Проверка размера файла 
+function CheckSize($fileSize, $maxFileSize = 5000000)
+{
+    return ($fileSize > $maxFileSize) ? false : true;
+}
+
+
+if (isset($_FILES['userfile']) && isset($_POST['upload'])) {
+    $files = FilesArr($_FILES['userfile']);
     $uploadPath = $_SERVER['DOCUMENT_ROOT'] . '/upload/';
     $fileTypesAccept = ['image/jpeg', 'image/png', 'image/jpg'];
-    //Проверяем ограничение по количеству файлов 
-    if (count($_FILES['userfile']) > 5) {
+    
+    if (CountFiles($files) === false) {
         $results[] = ("Нельзя загружать более 5 файлов");
-    }
-    //Проверяем каждый файл поочереди
-    foreach ($_FILES['userfile'] as $k => $v) {
-        $fileName = $_FILES['userfile'][$k]['name'];
-        $fileTmpName = $_FILES['userfile'][$k]['tmp_name'];
-        $fileType = $_FILES['userfile'][$k]['type'];
-        $fileSize = $_FILES['userfile'][$k]['size'];
-        $errorCode = $_FILES['userfile'][$k]['error'];
-        //Проверяем наличие ошибок
-        if ($errorCode != 0) {
-            $results[] = "Ошибка при загрузке файлов";
-        }
-        //Проверяем тип файла 
-        if (!in_array($fileType,$fileTypesAccept)) {
-            $results[] = "У файла $fileName недопустимый тип";
-        }
-        //Проверяем ограничение по размеру
-        if ($fileSize > 5000000) {
-            $results[] = "Размер изображения $fileName превышает 5 Мбайт";
-        }
-        //Если не возникло ошибок, загружаем файл
-        if (empty($results)) {
-            move_uploaded_file($fileTmpName, $uploadPath . $fileName);
-        }
-    }
-    if (empty($results)) {
-        $results[] = "Фотографии успешно загружены";
-    }
+    } else {
+        foreach ($files as $k => $v) {
+            $fileName = $files[$k]['name'];
+            $fileTmpName = $files[$k]['tmp_name'];
+            $fileType = $files[$k]['type'];
+            $fileSize = $files[$k]['size'];
+            $errorCode = $files[$k]['error'];
 
+            if (CheckErrors($errorCode) === false) {
+                $results[] = "Ошибка при загрузке файлов";
+            }
+
+            if (CheckType($fileType) === false) {
+                $results[] = "У файла $fileName недопустимый тип";
+            }
+
+            if (CheckSize($fileSize) === false) {
+                $results[] = "Размер изображения $fileName превышает 5 Мбайт";
+            }
+            if (empty($results)) {
+                move_uploaded_file($fileTmpName, $uploadPath . $fileName);
+            }
+        }
+        if (empty($results)) {
+            $results[] = "Фотографии успешно загружены";
+        }        
+    }
 }
+
 
